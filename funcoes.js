d71428d450a934655532d60e00ef63fd725f7398
Raissa de Campos <camposraissa311@gmail.com>
2022-05-16 20:37:53 -0300

    filmes

---
+++ funcoes.js
@@ -0,0 +1,9 @@
+
+var imagens = ["imagens/slide1.jpg", "imagens/slide2.jpg", "imagens/slide3.jpg", "imagens/slide4.jpg", "imagens/slide5.jpg", "imagens/slide6.jpg"];
+var imagematual = 0;
+
+function trocaimagem() {
+ imagematual = (imagematual + 1) % 6;
+document.querySelector('.gif img'). src = imagens[imagematual];
+}
+setInterval(trocaimagem, 1500);
